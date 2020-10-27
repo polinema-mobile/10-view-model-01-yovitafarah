398f90b3bf0fcdaefee9aa2b83936daf34a35939
yovitafarah <yovitafarah19@gmail.com>
2020-10-27 15:16:26 +0700

    Tugas reset

--- app/src/main/java/id/putraprima/courtcounter/viewmodels/CounterViewModels.java
+++ app/src/main/java/id/putraprima/courtcounter/viewmodels/CounterViewModels.java
@@ -30,4 +30,12 @@ public class CounterViewModels extends ViewModel {
     }
 
     //TODO : Tambahkan Implementasi View Model Untuk Reset Point
+
+    public void resetPointScore(){
+        score.setAwayScore(0);
+        score.setHomeScore(0);
+        scoreMutableLiveData.setValue(score);
+        Log.d("Point Away : ", String.valueOf(score.getAwayScore()));
+        Log.d("Point Home : ", String.valueOf(score.getHomeScore()));
+    }
 }
